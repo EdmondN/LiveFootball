537cded6f1da09f7ca161a704429c8171631e5c3
Edmond Nanushaj <33865566+EdmondN@users.noreply.github.com>
2018-02-22 13:14:44 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/android/livefootball/MainActivity.java
+++ app/src/main/java/com/example/android/livefootball/MainActivity.java
@@ -6,12 +6,12 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
-    int scoreRedTeamA = 0;
-    int scoreRedTeamB = 0 ;
-    int scoreFoulTeamA = 0;
-    int scoreFoulTeamB = 0;
+    int scoreTeamA;
+    int scoreTeamB;
+    int scoreRedTeamA;
+    int scoreRedTeamB;
+    int scoreFoulTeamA;
+    int scoreFoulTeamB;
 
 
     @Override
@@ -24,68 +24,60 @@ public class MainActivity extends AppCompatActivity {
         displayRedForTeamB(0);
         displayFoulForTeamA(0);
         displayFoulForTeamB(0);
-
     }
 
     /**
      * Add +1 Goal score for Team A.
      */
-
-    public void addGoalTeamA (View V){
-        scoreTeamA = scoreTeamA + 1;
-        displayForTeamA (scoreTeamA);
-
-
+    public void addGoalTeamA(View V) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
      * Add +1 Red Card score for Team A.
      */
-    public void redForTeamA (View V){
-        scoreRedTeamA = scoreRedTeamA + 1;
-        displayRedForTeamA (scoreRedTeamA);
-
+    public void redForTeamA(View V) {
+        scoreRedTeamA += 1;
+        displayRedForTeamA(scoreRedTeamA);
     }
 
     /**
      * Add +1 Foul score for Team A.
      */
-    public void addFoulForTeamA (View V){
-        scoreFoulTeamA = scoreFoulTeamA + 1;
-        displayFoulForTeamA (scoreFoulTeamA);
-
-
+    public void addFoulForTeamA(View V) {
+        scoreFoulTeamA += 1;
+        displayFoulForTeamA(scoreFoulTeamA);
     }
+
     /**
      * Add +1 Goal score for Team B.
      */
-    public void addGoalTeamB (View V){
-        scoreTeamB = scoreTeamB + 1;
-        displayForTeamB (scoreTeamB);
-
+    public void addGoalTeamB(View V) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
     }
+
     /**
      * Add +1 Red Card score for Team B.
      */
-    public void redForTeamB (View V){
-        scoreRedTeamB = scoreRedTeamB + 1;
-        displayRedForTeamB (scoreRedTeamB);
-
-
+    public void redForTeamB(View V) {
+        scoreRedTeamB += 1;
+        displayRedForTeamB(scoreRedTeamB);
     }
+
     /**
      * Add +1 Foul score for Team B.
      */
-    public void addFoulForTeamB (View V){
-        scoreFoulTeamB = scoreFoulTeamB + 1;
-        displayFoulForTeamB (scoreFoulTeamB);
-
+    public void addFoulForTeamB(View V) {
+        scoreFoulTeamB += 1;
+        displayFoulForTeamB(scoreFoulTeamB);
     }
 
     /**
      * Reset Score.
      */
-    public void resetScore (View V){
+    public void resetScore(View V) {
         scoreTeamA = 0;
         scoreTeamB = 0;
         scoreRedTeamA = 0;
@@ -104,47 +96,47 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given Goal score for Team A.
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+        TextView scoreViewGoalTeamA = (TextView) findViewById(R.id.team_a_score);
+        scoreViewGoalTeamA.setText(String.valueOf(score));
     }
 
     /**
      * Displays the given Goal score for Team B.
      */
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
+        TextView scoreViewGoalTeamB = (TextView) findViewById(R.id.team_b_score);
+        scoreViewGoalTeamB.setText(String.valueOf(score));
     }
 
     /**
      * Displays the given Red Card score for Team A.
      */
     public void displayRedForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_red_score);
-        scoreView.setText(String.valueOf(score));
+        TextView scoreViewRedTeamA = (TextView) findViewById(R.id.team_a_red_score);
+        scoreViewRedTeamA.setText(String.valueOf(score));
     }
 
     /**
      * Displays the given Red Card score for Team B.
      */
     public void displayRedForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_red_score);
-        scoreView.setText(String.valueOf(score));
+        TextView scoreViewRedTeamB = (TextView) findViewById(R.id.team_b_red_score);
+        scoreViewRedTeamB.setText(String.valueOf(score));
     }
 
     /**
      * Displays the given Foul score for Team A.
      */
     public void displayFoulForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_foul_score);
-        scoreView.setText(String.valueOf(score));
+        TextView scoreViewFoulTeamA = (TextView) findViewById(R.id.team_a_foul_score);
+        scoreViewFoulTeamA.setText(String.valueOf(score));
     }
 
     /**
      * Displays the given Foul score for Team B.
      */
     public void displayFoulForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_foul_score);
-        scoreView.setText(String.valueOf(score));
+        TextView scoreViewFoulTeamB = (TextView) findViewById(R.id.team_b_foul_score);
+        scoreViewFoulTeamB.setText(String.valueOf(score));
     }
 }
